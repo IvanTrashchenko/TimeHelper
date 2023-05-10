8d343e2eece3459c8f951b226a242849c7ca6b5c
qbq-itr <ivan.trashchenko@quibiq.ch>
2023-05-10 13:26:31 +0300

    Db: Update Date table constraints

--- TimeHelper.Data.Database/Table/Date.sql
+++ TimeHelper.Data.Database/Table/Date.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Date]
 (
-	[DateId] INT NOT NULL PRIMARY KEY, 
+	[DateId] INT NOT NULL IDENTITY, 
     [DateName] NVARCHAR(256) NOT NULL, 
-    [DateValue] DATE NOT NULL
+    [DateValue] DATE NOT NULL,
+    CONSTRAINT [PK_Date] PRIMARY KEY ([DateId]),
+    CONSTRAINT UC_DateName UNIQUE ([DateName])
 )
